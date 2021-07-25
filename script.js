b6fff1d808c25a5d6720d48d3a054323493e50cd
Anna Matvyeyenko <anna.matvyeyenko@gmail.com>
2021-07-25 02:20:26 +0200

    popup open

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ let openPopup = document.querySelector('.popup');
 let closePopup = document.querySelector('.popup__close');
 
 editButton.addEventListener('click', function () {
+  let formElement = document.querySelector('.popup__container');
+  let nameInput = formElement.querySelector('.popup__name');  // Воспользуйтесь инструментом .querySelector()
+  let jobInput = formElement.querySelector('.popup__about');
+  let nameElement = document.querySelector('.profile__name');
+  let jobElement = document.querySelector('.profile__about'); 
+  nameInput.value = nameElement.textContent;
+  jobInput.value = jobElement.textContent;
   openPopup.classList.add('popup_opened');
 });
 
-closePopup.addEventListener('click', function () {
+closePopup.addEventListener('click', function (event) {
+  event.preventDefault();
   openPopup.classList.remove('popup_opened');
 });
 
@@ -15,7 +23,7 @@ closePopup.addEventListener('click', function () {
 let formElement = document.querySelector('.popup__container'); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
 let nameInput = formElement.querySelector('.popup__name'); // Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.popup__about') // Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector('.popup__about'); // Воспользуйтесь инструментом .querySelector()
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -24,11 +32,18 @@ function formSubmitHandler(event) {
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
 
-  console.log(nameInput.value) // Получите значение полей jobInput и nameInput из свойства value
+ // Получите значение полей jobInput и nameInput из свойства value
 
-    // Выберите элементы, куда должны быть вставлены значения полей
 
-    // Вставьте новые значения с помощью textContent
+  let nameElement = document.querySelector('.profile__name');
+  let jobElement = document.querySelector('.profile__about'); // Выберите элементы, куда должны быть вставлены значения полей
+
+   // Вставьте новые значения с помощью textContent
+  nameElement.textContent = nameInput.value;
+  jobElement.textContent = jobInput.value;
+
+  openPopup.classList.remove('popup_opened');
+
 }
 
 // Прикрепляем обработчик к форме:
