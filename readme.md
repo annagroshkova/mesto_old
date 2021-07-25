8c88180a586526436fb5ca8bc59940bc02e220fd
Anna Matvyeyenko <anna.matvyeyenko@gmail.com>
2021-07-25 02:30:42 +0200

    readme, prettier

--- readme.md
+++ readme.md
@@ -1,5 +1,15 @@
 # Mesto
 
-> in progress
+> Каталог интересных мест
+
+https://annamatvyeyenko.github.io/mesto/
+
+## Технологии
+
+- BEM
+- HTML
+- CSS (Grid, Flexbox)
+- JavaScript (!)
+- Github Pages
 
 (c) Anna Groshkova
--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-let editButton = document.querySelector('.profile__edit-button');
-let popupElement = document.querySelector('.popup');
-let closePopupButton = document.querySelector('.popup__close');
+let editButton = document.querySelector(".profile__edit-button");
+let popupElement = document.querySelector(".popup");
+let closePopupButton = document.querySelector(".popup__close");
 
-editButton.addEventListener('click', function () {
-  let formElement = document.querySelector('.popup__container');
-  let nameInput = formElement.querySelector('.popup__name');
-  let jobInput = formElement.querySelector('.popup__about');
-  let nameElement = document.querySelector('.profile__name');
-  let jobElement = document.querySelector('.profile__about');
+editButton.addEventListener("click", function () {
+  let formElement = document.querySelector(".popup__container");
+  let nameInput = formElement.querySelector(".popup__name");
+  let jobInput = formElement.querySelector(".popup__about");
+  let nameElement = document.querySelector(".profile__name");
+  let jobElement = document.querySelector(".profile__about");
   nameInput.value = nameElement.textContent;
   jobInput.value = jobElement.textContent;
-  popupElement.classList.add('popup_opened');
+  popupElement.classList.add("popup_opened");
 });
 
-closePopupButton.addEventListener('click', function (event) {
+closePopupButton.addEventListener("click", function (event) {
   event.preventDefault();
-  popupElement.classList.remove('popup_opened');
+  popupElement.classList.remove("popup_opened");
 });
 
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__about');
+let formElement = document.querySelector(".popup__container");
+let nameInput = formElement.querySelector(".popup__name");
+let jobInput = formElement.querySelector(".popup__about");
 
 function formSubmitHandler(event) {
   event.preventDefault();
 
-  let nameElement = document.querySelector('.profile__name');
-  let jobElement = document.querySelector('.profile__about');
+  let nameElement = document.querySelector(".profile__name");
+  let jobElement = document.querySelector(".profile__about");
 
   nameElement.textContent = nameInput.value;
   jobElement.textContent = jobInput.value;
 
-  popupElement.classList.remove('popup_opened');
+  popupElement.classList.remove("popup_opened");
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener("submit", formSubmitHandler);
