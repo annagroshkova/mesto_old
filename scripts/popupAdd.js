16f344f5d0b0a2f913b4e394f56d3fa6b9c1b732
Anna Matvyeyenko <anna.matvyeyenko@gmail.com>
2021-08-07 20:17:43 +0200

    popup add

---
+++ scripts/popupAdd.js
@@ -0,0 +1,44 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+let addButton = document.querySelector(".profile__add-button");
+let popupAdd = document.querySelector(".popup-add");
+let closePopupAddButton = popupAdd.querySelector(".popup-add__close");
+
+function addClick () {
+  popupAdd.classList.add("popup_opened");
+}
+
+function closePopupAdd () {
+  popupAdd.classList.remove("popup_opened");
+}
+
+
+addButton.addEventListener("click", addClick);
+closePopupAddButton.addEventListener("click", closePopupAdd);
+
+
