b3c08a26ff6e2b8cdbec8529be88766a932b7819
Anna Matvyeyenko <anna.matvyeyenko@gmail.com>
2021-08-22 00:54:01 +0200

    review comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,35 @@
+const editButton = document.querySelector(".profile__edit-button");
+const popupElement = document.querySelector(".popup");
+const popupCloseButton = document.querySelector(".popup__close");
+const editFormElement = document.querySelector(".popup__form");
+const nameInput = editFormElement.querySelector(".popup__form-item_name_fullname");
+const jobInput = editFormElement.querySelector(".popup__form-item_name_occupation");
+const nameElement = document.querySelector(".profile__name");
+const jobElement = document.querySelector(".profile__about");
+
+function editClick () {
+  nameInput.value = nameElement.textContent.trim();
+  jobInput.value = jobElement.textContent.trim();
+  popupElement.classList.add("popup_opened");
+}
+
+function closePopup () {
+  popupElement.classList.remove("popup_opened");
+}
+
+function formSubmitHandler(event) {
+  event.preventDefault();
+
+  nameElement.textContent = nameInput.value.trim();
+  jobElement.textContent = jobInput.value.trim();
+
+  closePopup();
+}
+
+editButton.addEventListener("click", editClick);
+popupCloseButton.addEventListener("click", closePopup);
+editFormElement.addEventListener("submit", formSubmitHandler);
+
 
 const places = [
   {
@@ -28,26 +60,18 @@ const places = [
 ];
 
 
-
-
 const placesElement = document.querySelector('.places');
-const addButton = document.querySelector(".profile__add-button");
+const profileAddButton = document.querySelector(".profile__add-button");
 const popupAdd = document.querySelector(".popup_name_add");
-const closePopupAddButton = popupAdd.querySelector(".popup__close_name_add");
+const popupAddCloseButton = popupAdd.querySelector(".popup__close_name_add");
 
-const addFormElement = document.querySelector(".popup__form_name_add");
-const placeNameInput = addFormElement.querySelector(".popup__form-item_name_picture-title");
-const placeLinkImput = addFormElement.querySelector(".popup__form-item_name_picture-link");
+const formAddElement = document.querySelector(".popup__form_name_add");
+const placeNameInput = formAddElement.querySelector(".popup__form-item_name_picture-title");
+const placeLinkImput = formAddElement.querySelector(".popup__form-item_name_picture-link");
 const popupNameImage = document.querySelector('.popup_name_image');
-const closePopupImageButton = popupNameImage.querySelector('.popup__close_name_image');
+const popupCloseImageButton = popupNameImage.querySelector('.popup__close_name_image');
 const popupImage = document.querySelector('.popup__image');
 const popupImageTitle = document.querySelector('.popup__image-title');
-
-
-
-
-
-
 
 function addClick () {
   popupAdd.classList.add("popup_opened");
@@ -82,16 +106,14 @@ function updatePlaces() {
   placesElement.innerHTML = placesHTML
 }
 
+profileAddButton.addEventListener("click", addClick);
+
+popupAddCloseButton.addEventListener("click", closePopupAdd);
+
+popupCloseImageButton.addEventListener("click", closePopupNameImage);
 
 
-addButton.addEventListener("click", addClick);
-
-closePopupAddButton.addEventListener("click", closePopupAdd);
-
-closePopupImageButton.addEventListener("click", closePopupNameImage);
-
-
-addFormElement.addEventListener("submit", (event) => {
+formAddElement.addEventListener("submit", (event) => {
   event.preventDefault();
   places.unshift(
     {name: placeNameInput.value,
@@ -115,4 +137,23 @@ const placeButtons = document.querySelectorAll('.places__image-button');
   });
  })
 
+ const likeButtons = document.querySelectorAll('.places__like-button');
+ likeButtons.forEach (button => {
+  button.addEventListener('click', function (evt) {
+    const eventTarget = evt.target;
+    eventTarget.classList.toggle('places__like-button_active');
 
+  });
+ })
+
+ const deleteButtons = document.querySelectorAll('.places__delete-button');
+
+ function removePlace (evt) {
+  const placeCard = evt.target.closest('.places__place');
+  placeCard.remove();
+}
+
+
+deleteButtons.forEach (button => {
+  button.addEventListener('click', removePlace)
+});
